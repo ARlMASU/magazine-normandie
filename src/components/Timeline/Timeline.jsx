27d580e1e7f24b4d70aeba198cc651fcfd49ccd4
ARlMASU <MY_NAME@example.com>
2025-04-25 01:12:51 +0200

    timelineAdvancement

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -4,17 +4,21 @@ import Line from "./Line/Line";
 
 import { useEffect, useRef } from "react";
 
-export default function Timeline({timelineWidth}) {
-	const timelineRef = useRef(null);
-	// console.log(timelineRef.current.width);
+export default function Timeline({}) {
+  const timelineRef = useRef(null);
 
-	useEffect(() => {
-		timelineWidth = timelineRef.current.offsetWidth;
-	}, [timelineRef]);
+  useEffect(() => {
+    // requestAnimationFrame(() => {
+    if (timelineRef.current) {
+      const timelineWidth = timelineRef.current.offsetWidth;
+      console.log("timelineWidth = " + timelineWidth + "px");
+    }
+    // });
+  }, [timelineRef]);
 
-	return (
-		<div className={styles.timeline} ref={timelineRef}>
-			<Line width={8} height={120} opacity={1}></Line>
-		</div>
-	);
+  return (
+    <div className={styles.timeline} ref={timelineRef}>
+      <Line width={8} height={120} opacity={1}></Line>
+    </div>
+  );
 }
