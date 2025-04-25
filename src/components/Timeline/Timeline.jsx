155e771263c243ff886b3094cf344b0c3640b2e9
Arimasu <onurefekocak68@gmail.com>
2025-04-25 11:10:24 +0200

    timelineAdvancement2

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -2,23 +2,26 @@ import styles from "./Timeline.module.scss";
 
 import Line from "./Line/Line";
 
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, createElement } from "react";
 
 export default function Timeline({}) {
-  const timelineRef = useRef(null);
+	const timelineRef = useRef(null);
 
-  useEffect(() => {
-    // requestAnimationFrame(() => {
-    if (timelineRef.current) {
-      const timelineWidth = timelineRef.current.offsetWidth;
-      console.log("timelineWidth = " + timelineWidth + "px");
-    }
-    // });
-  }, [timelineRef]);
+	useEffect(() => {
+		if (timelineRef.current) {
+			const timelineCenterX =
+				(timelineRef.current.offsetLeft * 2 + timelineRef.current.offsetWidth) /
+				2;
+		}
+	}, [timelineRef]);
 
-  return (
-    <div className={styles.timeline} ref={timelineRef}>
-      <Line width={8} height={120} opacity={1}></Line>
-    </div>
-  );
+	function handleDistanceFromCenter() {
+		return createElement(Line, { width: 8, height: 120, opacity: 1 });
+	}
+
+	return (
+		<div className={styles.timeline} ref={timelineRef} onScroll={handleDistanceFromCenter()}>
+			<Line width={8} height={120} opacity={1} ref={lineRef}></Line>
+		</div>
+	);
 }
