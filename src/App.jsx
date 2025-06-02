8a60b432117c75e33092663bc70b3b3ae5c6dfea
Arimasu <onurefekocak68@gmail.com>
2025-06-02 08:54:55 +0200

    DeletedCommentedLines

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,12 @@ import { Outlet } from "react-router-dom";
 import styles from "./App.module.scss";
 
 import Nav from "./components/Nav/Nav";
-// import Archives from "./pages//Archives/Archives";
-
-import { useState, useEffect } from "react";
 
 export default function App() {
-
-  return (
-    <main className={styles.app}>
-      <Nav />
-      <Outlet />
-    </main>
-  );
+	return (
+		<main className={styles.app}>
+			<Nav />
+			<Outlet />
+		</main>
+	);
 }
--- src/components/Post/Filter/Filter.jsx
+++ src/components/Post/Filter/Filter.jsx
@@ -3,7 +3,9 @@ import styles from "./Filter.module.scss";
 export default function Filter({ children, fontSize }) {
 	return (
 		<div className={styles.filter}>
-			<p className={styles.filter__text} style={{fontSize: `${fontSize}rem`}}>{children}</p>
+			<p className={styles.filter__text} style={{ fontSize: `${fontSize}rem` }}>
+				{children}
+			</p>
 		</div>
 	);
 }
